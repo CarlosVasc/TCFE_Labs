b63ea3f2c14105343c20061197315a72e4245eff
carlos <carlos.de.vasconcelos@tecnico.ulisboa.pt>
2021-06-08 05:25:59 +0100

    t5 sem sim

--- t5/mat/rc.m
+++ t5/mat/rc.m
@@ -1,88 +1,72 @@
 close all
 clear all
-pkg load symbolic
-pkg load control
-pkg load signal
-%--------------------  DADOS  ----------------------- 
-
-Vfonte = 230;
-f=50;
-w=2*pi*f;
-
-R1 = 10000;
-C = 10e-6;
-R2 = 1000;
-voltas = 17;
-
-A = Vfonte/voltas;
-
-%------------------------Envelope Detector----------------------
-t=linspace(0, 0.2, 1000);
-
-vs=A*cos(w*t); %depois de transformador
-
-toff = 1/w * atan(1/(w*R1*C));
-
-v_fex = A*cos(w*toff)*exp(-(t-toff)/(R1*C));
-
-for i=1:length(t)
-	  v0hr(i) = abs(vs(i));
-end
-
-for i=1:length(t)
-  if t(i) < toff
-    v0(i) = v0hr(i);
-  elseif v_fex(i) > v0hr(i)
-    v0(i) = v_fex(i);
-  else 
-    toff = toff + 1/(2*f) ;
-    v_fex = A*abs(cos(w*toff))*exp(-(t-toff)/(R1*C));
-    v0(i) = v0hr(i);
-  end
-end
-
-average = mean(v0);
-ripple = max(v0) - min(v0);
+clc
 
 
-%-------------------------Voltage Regulator----------------------------------
+%------------------------
+f = logspace(0,8,800);
 
-n_diodes = 19;
-Von = 0.632;
-
-v0_2_dc = Von*n_diodes;
-
-vt = 0.025;
-Is = 1e-14;
-n = 1;
-
-Rd = n*vt/(Is*exp(Von/(n*vt)))
-
-for i = 1:length(t)
-    v0_2_ac(i) = n_diodes*Rd/(n_diodes*Rd+R2) * (v0(i)-average);
-end
-
-v0_2 = v0_2_dc + v0_2_ac;
-
-average_reg = mean(v0_2)
-ripple_reg = max(v0_2)-min(v0_2) 
-
-cost = R1/1000 + R2/1000 + C*1e6 + (n_diodes+0.4)*0.1; 
-MERIT = 1/(cost*(ripple_reg + abs(average_reg - 12) + 1e-6))
+C1 = 220e-9
+C2 = 110e-9
+R1 = 1e3
+R2 = 1e3
+R3 = 1e3
+R4 = 100e3
 
 
-%plots ----
-fid1 = figure();
-plot (t*1000, vs, ";vs transformer;", t*1000,v0, ";vo envelope;", t*1000,v0_2, ";vo regulator;");
-xlabel ("t[ms]")
-ylabel ("v_O [Volts]")
-legend('Location','southeast');
-print (fid1, "V_todas.eps", "-depsc");
+gain = (R1.*2.*pi.*f.*C1./(1+R1.*j.*2.*pi.*f.*C1)).*(1 + R4/R3).*(1./(j.*2.*pi.*f.*C2.*R2 + 1));
+gain_db=20*log10(abs(gain));
+
+I_max=find (gain_db==max(gain_db));
+f_max=f(I_max);
+
+gain_max=max(gain);
+gainDB_max=max(gain_db);
+Z_in = (R1 + 1/(j*2*pi*f_max*C1))
+Z_out = R2/(j*2*pi*f_max*C2)/(R2+1/(j*2*pi*f_max*C2))
+
+Zin_a=abs(Z_in)
+Zout_a=abs(Z_out)
+
+%---------------------
+G_desvio =abs(100- max(abs(gain)));
+F_desvio=abs(1000-f_max);
+cost_amp = ((8.661e-12 + 30e-12)*1e6 + (100000 + 5305 + 5305 + 1836 + 1836 + 13190000 + 50 + 100 + 18160)/1000 + (2*0.1)) + (0.220*2)
+cost=cost_amp+(1e-3*(R1 + R2 + R3 + R4)+1e6*(C1 + C2))
+Merit = 1/(cost * (G_desvio + F_desvio+1e-6))
+%------------- Export ------------
+fid = figure ();
+semilogx(f, gain_db);
+title("Gain");
+legend("v_o(f)/v_i(f)");
+ylabel("Gian (dB)")
+xlabel("frequency (Hz)")
+print(fid ,"Gain_teo.eps","-depsc");
 
 
-fid2 = figure();
-plot (t*1000,v0_2-12);
-xlabel ("t[ms]")
-ylabel ("v_O [Volts]")
-print (fid2, "diferença.eps", "-depsc");
+fid2=fopen("valores.tex","w+");
+fprintf(fid2,"C1 & %e\\\\ \\hline \n\
+C2 & %.5e\\\\ \\hline \n\
+R1 & %.5e\\\\ \\hline \n\
+R2 & %.5e\\\\ \\hline \n\
+R3 & %.5e\\\\ \\hline \n\
+R4 & %.5e\\\\ \\hline", C1,C2,R1,R2,R3,R4)
+fclose(fid2);
+
+
+fid3=fopen("merit.tex","w+");
+fprintf(fid3,"Gain Deviation & %f\\\\ \\hline \n\
+Frequency deviation & %.f\\\\ \\hline \n\
+Cost & %.5e\\\\ \\hline \n\
+Merit & %.5e\\\\ \\hline", G_desvio,F_desvio,cost,Merit)
+fclose(fid3);
+
+
+fid4=fopen("Val_meio.tex","w+");
+fprintf(fid4,"Gain & %.5e\\\\ \\hline \n\
+Gain (db) & %.5e\\\\ \\hline \n\
+Input impedance &  %.5e + %.5ej\\\\ \\hline \n\
+Output impedance &  %e + %ej\\\\ \\hline", gain_max,gainDB_max,real(Z_in), imag(Z_in),real(Z_out) , imag(Z_out))
+fclose(fid4);
+
 
